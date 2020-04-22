b5e7e05def009fb63fe43d21c70bd0077df691c4
tzw1992 <289210328@qq.com>
2020-04-22 09:26:10 +0800

    2020-04-22 9:25天提交

--- Chapter12/src/main/java/com/course/config/ExtendTestNGIReporterListener.java
+++ Chapter12/src/main/java/com/course/config/ExtendTestNGIReporterListener.java
@@ -116,6 +116,7 @@ public class ExtendTestNGIReporterListener implements IReporter {
         htmlReporter.config().setTestViewChartLocation(ChartLocation.TOP);
         htmlReporter.config().setTheme(Theme.STANDARD);
         htmlReporter.config().setCSS(".node.level-1  ul{ display:none;} .node.level-1.active ul{display:block;}");
+        htmlReporter.config().setEncoding("gbk");
         extent = new ExtentReports();
         extent.attachReporter(htmlReporter);
         extent.setReportUsesManualConfiguration(true);
